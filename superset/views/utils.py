301ad623c9f11af9e0278f849b86d7be21eee022
Reese <10563996+reesercollins@users.noreply.github.com>
2022-07-06 10:27:50 -0400

    fix: Allow dataset owners to explore their datasets (#20382)
    * fix: Allow dataset owners to explore their datasets
    
    * Re-order imports
    
    * Give owners security manager permissions to their datasets
    
    * Update test suite
    
    * Add SqlaTable to is_owner types
    
    * Add owners to datasource mock
    
    * Fix VSCode import error
    
    * Fix merge error

--- superset/views/utils.py
+++ superset/views/utils.py
@@ -32,6 +32,7 @@ from sqlalchemy.orm.exc import NoResultFound
 import superset.models.core as models
 from superset import app, dataframe, db, result_set, viz
 from superset.common.db_query_status import QueryStatus
+from superset.connectors.sqla.models import SqlaTable
 from superset.datasource.dao import DatasourceDAO
 from superset.errors import ErrorLevel, SupersetError, SupersetErrorType
 from superset.exceptions import (
