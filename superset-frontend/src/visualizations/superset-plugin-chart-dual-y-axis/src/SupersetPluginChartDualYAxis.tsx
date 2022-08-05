679f637267b75169b847b98b52db6dd3378acf8e
gunashekar <gunashekar.c@nineleaps>
2022-08-05 15:48:42 +0530

    alignment changes for dual y-axis

--- superset-frontend/src/visualizations/superset-plugin-chart-dual-y-axis/src/SupersetPluginChartDualYAxis.tsx
+++ superset-frontend/src/visualizations/superset-plugin-chart-dual-y-axis/src/SupersetPluginChartDualYAxis.tsx
@@ -61,7 +61,7 @@ function SupersetPluginChartDualYAxis(
   const barChartConfig = (name: any) => ({
     nameLocation: 'center',
     nameTextStyle: {
-      padding: [15, 0],
+      padding: [0, 0, 50, 0],
     },
     type: 'value',
     name,
@@ -75,7 +75,7 @@ function SupersetPluginChartDualYAxis(
   const lineChartConfig = (name: any) => ({
     nameLocation: 'center',
     nameTextStyle: {
-      padding: [10, 0],
+      padding: [25, 0, 0, 0],
     },
     type: 'value',
     name,
@@ -115,14 +115,9 @@ function SupersetPluginChartDualYAxis(
       },
     },
     grid: {
-      right: '20%',
-    },
-    toolbox: {
-      feature: {
-        dataView: { show: true, readOnly: false },
-        restore: { show: true },
-        saveAsImage: { show: true },
-      },
+      bottom: '10%',
+      left: '20%',
+      right: '15%',
     },
     legend: {
       data: [...formData.cols, ...formData.xValues],
