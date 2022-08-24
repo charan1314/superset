d2179d577763c19673efd127691d08fbfe6748b4
gunashekar <gunashekar.c@nineleaps>
2022-08-24 20:33:16 +0530

    increasing the space between bars in the bars chart

--- superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
+++ superset-frontend/plugins/legacy-preset-chart-nvd3/src/NVD3Vis.js
@@ -413,7 +413,7 @@ function nvd3Vis(element, props) {
         chart = nv.models
           .multiBarChart()
           .showControls(showControls)
-          .groupSpacing(0.6);
+          .groupSpacing(0.45);
 
         if (!reduceXTicks) {
           width = computeBarChartWidth(data, isBarStacked, maxWidth);
@@ -428,7 +428,7 @@ function nvd3Vis(element, props) {
           .multiBarChart()
           .showControls(showControls)
           .reduceXTicks(reduceXTicks)
-          .groupSpacing(0.6); // Distance between each group of bars.
+          .groupSpacing(0.45); // Distance between each group of bars.
 
         chart.xAxis.showMaxMin(false);
 
