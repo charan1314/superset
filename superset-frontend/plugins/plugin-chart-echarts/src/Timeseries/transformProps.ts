506ec081b93ba59c2b3eec979b1eb0e304cc2b18
gunashekar <gunashekar.c@nineleaps>
2022-09-22 17:46:39 +0530

    modifications for markLine || Line charts of echarts

--- superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
+++ superset-frontend/plugins/plugin-chart-echarts/src/Timeseries/transformProps.ts
@@ -1,5 +1,3 @@
-// @ts-nocheck
-/* eslint-disable */
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -33,7 +31,6 @@ import {
   isTimeseriesAnnotationLayer,
   TimeseriesChartDataResponseResult,
   t,
-  TimeGranularity,
 } from '@superset-ui/core';
 import { isDerivedSeries } from '@superset-ui/chart-controls';
 import { EChartsCoreOption, SeriesOption } from 'echarts';
@@ -134,6 +131,7 @@ export default function transformProps(
     zoomable,
     richTooltip,
     xAxis: xAxisOrig,
+    xAxisLabelRotation,
     emitFilter,
     groupby,
     showValue,
@@ -144,9 +142,7 @@ export default function transformProps(
     xAxisTitleMargin,
     yAxisTitleMargin,
     yAxisTitlePosition,
-    xAxisLabelRotation,
     sliceId,
-    timeGrainSqla,
     orientation,
   }: EchartsTimeseriesFormData = { ...DEFAULT_FORM_DATA, ...formData };
 
@@ -331,6 +327,38 @@ export default function transformProps(
     )
     .map(entry => entry.name || '')
     .concat(extractAnnotationLabels(annotationLayers, annotationData));
+
+  const markLineLabel: any = {
+    show: true,
+    position: 'start',
+    formatter: '{b} {c}',
+    lineHeight: 12,
+    padding: [0, 0, 0, 4.4],
+    distance: [0, -0.5],
+    width: 30,
+    // eslint-disable-next-line theme-colors/no-literal-colors
+    color: '#8d8d8d',
+    overflow: 'break',
+  };
+
+  const transperantMarkLine: any = {
+    normal: {
+      type: 'dashed',
+      // eslint-disable-next-line theme-colors/no-literal-colors
+      color: 'rgba(255,255,255,0)',
+      cap: 'butt',
+    },
+  };
+
+  const coloredMarkLine: any = {
+    normal: {
+      type: 'dashed',
+      // eslint-disable-next-line theme-colors/no-literal-colors
+      color: '#b2b2b2',
+      cap: 'butt',
+    },
+  };
+
   // eslint-disable-next-line array-callback-return
   series.map(row => {
     // eslint-disable-next-line no-param-reassign
@@ -338,39 +366,58 @@ export default function transformProps(
       symbol: ['none', 'none'],
       data: [
         {
-          name: '2025',
-          xAxis: '2025',
-          label: {
-            position: 'start',
-            distance: [0, 7],
-            // eslint-disable-next-line theme-colors/no-literal-colors
-            color: '#8d8d8d',
-          },
-          lineStyle: {
-            normal: {
-              type: 'dashed',
-              // eslint-disable-next-line theme-colors/no-literal-colors
-              color: 'black',
-              cap: 'butt',
-            },
-          },
+          name: 'T',
+          xAxis: '2018',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
         },
         {
-          name: '2030',
+          name: 'T',
+          xAxis: '2020',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
+        },
+        {
+          name: 'T',
+          xAxis: '2025',
+          emphasis: {
+            disabled: true,
+          },
+          label: markLineLabel,
+          lineStyle: coloredMarkLine,
+        },
+        {
+          name: 'T',
           xAxis: '2030',
-          label: {
-            position: 'start',
-            distance: [0, 7],
-            // eslint-disable-next-line theme-colors/no-literal-colors
-            color: '#8d8d8d',
+          emphasis: {
+            disabled: true,
           },
-          lineStyle: {
-            normal: {
-              type: 'dashed',
-              // eslint-disable-next-line theme-colors/no-literal-colors
-              color: 'black',
-            },
-          },
+          label: markLineLabel,
+          lineStyle: coloredMarkLine,
+        },
+        {
+          name: 'T',
+          xAxis: '2035',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
+        },
+        {
+          name: 'T',
+          xAxis: '2040',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
+        },
+        {
+          name: 'T',
+          xAxis: '2045',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
+        },
+        {
+          name: 'T',
+          xAxis: '2050',
+          label: markLineLabel,
+          lineStyle: transperantMarkLine,
         },
       ],
     };
@@ -378,21 +425,15 @@ export default function transformProps(
   let xAxis: any = {
     type: xAxisType,
     name: xAxisTitle,
+    axisTick: { show: false },
     nameGap: convertInteger(xAxisTitleMargin),
     nameLocation: 'middle',
     axisLabel: {
+      show: false,
       hideOverlap: true,
       formatter: xAxisFormatter,
       rotate: xAxisLabelRotation,
     },
-    min: function (value: any) {
-      return value.min - 3600 * 1000 * 24 * 31 * 12 * 2;
-    },
-    minInterval:
-      xAxisType === AxisType.time && timeGrainSqla
-        ? TIMEGRAIN_TO_TIMESTAMP[timeGrainSqla]
-        : 0,
-    maxInterval: TIMEGRAIN_TO_TIMESTAMP[TimeGranularity.FIVE_YEAR],
   };
 
   if (xAxisType === AxisType.time) {
@@ -429,6 +470,7 @@ export default function transformProps(
     grid: {
       ...defaultGrid,
       ...padding,
+      bottom: 40,
     },
     xAxis,
     yAxis,
